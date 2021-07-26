736bfd72207e74c46953eb7404dcc36c87fe5bd3
skrevolve <sukyu0919@naver.com>
2021-07-26 15:50:17 +0900

    [upload] finish

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('contacts/base');
+    return view('base');
 });
 
 // Route::resource('contacts', 'ContactController');
