f23b41079ebb99f2e07f93487513b807813cd802
skrevolve <sukyu0919@naver.com>
2021-07-26 13:59:50 +0900

    [upload] new

--- app/Contact.php
+++ app/Contact.php
@@ -6,12 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    protected $fillable = [
-        'first_name',
-        'last_name',
-        'email',
-        'job_title',
-        'city',
-        'country'
-    ];
+    //
 }
